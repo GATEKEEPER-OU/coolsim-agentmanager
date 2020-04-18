9e14ad4fda9f3bfa6a189a7de229d2311b3db034
Alessio Antonini <dottorpollo@gmail.com>
2020-04-18 10:22:33 +0100

    init

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+
+
+// todo definition of world clock
+// phases of the day
+
+// todo binding between movements and phases of the day
+
+// todo binding between events and phases of the day
+
+//
