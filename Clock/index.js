ef35c31f1622cc4894b97924ae458dfffed5b3db
Alessio Antonini <dottorpollo@gmail.com>
2020-04-18 23:40:42 +0100

    clock class generating the simulation time

--- Clock/index.js
+++ Clock/index.js
@@ -1,4 +1,90 @@
-// todo create community clock ( generator )
-// todo define ratio 1s x 10, 20, ...
+const moment = require('moment');
 
-// todo initialises and returns a generator community clock
+
+exports.init = (ratio) => new Clock(ratio);
+
+
+class Clock {
+    // ratio 60 => 1sec = 1min
+    // ratio 3600 => 1sec = 1h
+    // ratio 86400 => 1sec = 1day
+    // ...
+    constructor(ratio = 1) {
+        this.ratio = ratio;
+        // get delta between the current year and X ratio timestamps
+        this.delta = (Date.now() * this.ratio) - Date.now();
+
+        // init map of phases of the day
+        this.phases = new Map();
+        for(let i = 0 ; i < 24; i++){
+            if (i > 6 && i <= 12) {
+                this.phases.set(i, 'morning');
+            } else if(i > 12 && i <= 17) {
+                this.phases.set(i, 'afternoon');
+            } else if(i > 17 && i <= 21) {
+                this.phases.set(i, 'evening');
+            } else {
+                    this.phases.set(i,'night');
+            }
+        }
+        // console.log(this.phases);
+    }
+
+    // getters
+    // current time in the simulation
+    get now() {
+        // return current data X ratio - delta (so the clock starts at the real date)
+        return moment( (Date.now() * this.ratio) - this.delta);
+    }
+    // phase of the day in the simulation
+    get phase() {
+        let h = (this.now.get('hours') +1) % 24;
+        return this.phases.get( h);
+    }
+    // time interval converted to the time of the simulation,
+    // e.g. 5 hours = (5*3600s / ratio) * 1000 => sleep interval in milliseconds
+    // errors is used to introduce an error
+    // eg errors = 0.1 => up to +10%, error = -0.5 => up to minus 50%
+    // errors can be an array of possible errors
+    // Array of errors is not cumulative, only one will be randomly selected and applied
+    // eg errors = [0.1,-0.3] => up to 10% or minus to -30%
+    milliseconds(duration = 0, type = 'minutes', errors = 0) {
+        // calc of errors
+        let deviation = 0;
+        if(Array.isArray(errors)) {
+            // pick a random in the array of errors
+            let i = Math.floor(Math.random() * errors.length);
+            deviation = errors[i]* Math.random();
+        } else if(!isNaN(errors)){
+            deviation = errors * Math.random();
+        }
+        duration = duration + duration*deviation;
+        // console.log(duration);
+        // end of calc of errors
+
+        switch(type){
+            case 'months':
+                duration = duration * 30 * 24 * 60 * 60 * 1000;
+                break;
+            case 'days':
+                duration = duration * 24 * 60 * 60 * 1000;
+                break;
+            case 'hours':
+                duration = duration * 60 * 60 * 1000;
+                break;
+            case 'minutes':
+                duration = duration * 60 * 1000;
+                break;
+            default:
+                duration = duration * 1000;
+        }
+        // console.log(`duration in hours ${duration / (1000 * 3600)}`);
+
+        // apply simulation ratio
+        duration = duration / this.ratio;
+
+        return duration;
+    }
+}
+
+
--- app.js
+++ app.js
@@ -1,9 +1,26 @@
+const Clock = require('./Clock');
+
+
+
+// init of the simulation clock
+const timeSpeed = 3600; // 1s = 1h
+
+const clock = Clock.init(timeSpeed);
+
+//
+// setInterval(()=>{
+//     console.log(`${clock.phase} - ${clock.now.format("dddd, MMMM Do YYYY, H:mm:ss")}`);
+//     },1000);
+
+let sleepTime = 5;
+console.log(`taking a nap of ${sleepTime} hours, now are ${clock.now.format("dddd, MMMM Do YYYY, H:mm:ss")}`);
+setTimeout(()=>{
+    console.log(`waking up now at ${clock.now.format("dddd, MMMM Do YYYY, H:mm:ss")}`);
+}, clock.milliseconds(sleepTime, 'hours',[0.2,-0.2]));
 
 
 
 
-// todo definition of world clock
-// phases of the day
 
 // todo binding between movements and phases of the day
 
